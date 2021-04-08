905edcc46bc22d2ec31d72fa285049d21a3f504a
Justin Pfifer <justin.pfifer@carvana.com>
2021-04-08 08:53:30 -0700

    Preparing for the builder

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
 mod map;
+mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
 
-fn main() {
-    println!("Hello, world!");
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        self.player.update(ctx, &self.map);
+        ctx.cls();
+        self.map.render(ctx);
+        self.player.render(ctx);
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50().with_title("Dungeon Crawler").with_fps_cap(30.0).build()?;
+    main_loop(context, State::new())
 }
