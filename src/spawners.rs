34fa0982faca95edc3ddce9a6482f7c30c77b252
Justin Pfifer <justin.pfifer@carvana.com>
2021-04-09 08:56:50 -0700

    Completed the health system

--- src/spawners.rs
+++ src/spawners.rs
@@ -15,20 +15,27 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
     ));
 }
 
+fn goblin() -> (i32, String, FontCharType) {
+    (1, "Goblin".to_string(), to_cp437('g'))
+}
+
+fn orc() -> (i32, String, FontCharType) {
+    (2, "Orc".to_string(), to_cp437('o'))
+}
+
 pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
-    let (glyph, health) = match rng.range(0, 4) {
-        0 => (to_cp437('E'), 20),
-        1 => (to_cp437('O'), 15),
-        2 => (to_cp437('o'), 10),
-        _ => (to_cp437('g'), 3),
+    let (hp, name, glyph) = match rng.roll_dice(1, 10) {
+        1..=8 => goblin(),
+        _ => orc(),
     };
     ecs.push((
         Enemy,
         MovingRandomly,
         Health {
-            current: health,
-            max: health,
+            current: hp,
+            max: hp,
         },
+        Name(name),
         pos,
         Render {
             color: ColorPair::new(WHITE, BLACK),
