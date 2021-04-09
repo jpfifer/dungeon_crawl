06e8cd3f017bf270dcd7caeb2f8bcee0b00aeeb2
Justin Pfifer <justin.pfifer@carvana.com>
2021-04-08 17:23:00 -0700

    Added ECS

--- src/map.rs
+++ src/map.rs
@@ -4,66 +4,69 @@ const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum TileType {
-  Wall,
-  Floor,
+    Wall,
+    Floor,
 }
 
 pub fn map_index(x: i32, y: i32) -> usize {
-  (y * SCREEN_WIDTH + x) as usize
+    (y * SCREEN_WIDTH + x) as usize
 }
 
-
 pub struct Map {
-  pub tiles: Vec<TileType>,
+    pub tiles: Vec<TileType>,
 }
 
 impl Map {
-  pub fn new() -> Self {
-    Self {
-      tiles: vec![TileType::Floor; NUM_TILES]
-    }
-  }
-
-  pub fn in_bounds(&self, point: Point) -> bool {
-    point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
-  }
-
-  pub fn can_enter_tile(&self, point: Point) -> bool {
-    if let Some(idx) = self.try_idx(point) {
-      return self.tiles[idx] == TileType::Floor;
-    } else {
-      return false;
-    }
-  }
-
-  pub fn try_idx(&self, point: Point) -> Option<usize> {
-    if !self.in_bounds(point) {
-      None
-    } else {
-      Some(map_index(point.x, point.y))
-    }
-  }
-
-  pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
-    ctx.set_active_console(0);
-    for y in camera.top_y..camera.bottom_y {
-      for x in camera.left_x..camera.right_x {
-        if self.in_bounds(Point::new(x, y)) {
-          let idx = map_index(x, y);
-          match self.tiles[idx] {
-            TileType::Floor => {
-              ctx.set(x - camera.left_x,
-                      y - camera.top_y,
-                      WHITE,
-                      BLACK,
-                      to_cp437('.'))
-            }
-            TileType::Wall => {
-              ctx.set(x - camera.left_x, y - camera.top_y, WHITE, BLACK, to_cp437('#'))
-            }
-          }
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
         }
-      }
     }
-  }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        if let Some(idx) = self.try_idx(point) {
+            self.tiles[idx] == TileType::Floor
+        } else {
+            false
+        }
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_index(point.x, point.y))
+        }
+    }
+
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(0);
+        for y in camera.top_y..camera.bottom_y {
+            for x in camera.left_x..camera.right_x {
+                if self.in_bounds(Point::new(x, y)) {
+                    let idx = map_index(x, y);
+                    match self.tiles[idx] {
+                        TileType::Floor => ctx.set(
+                            x - camera.left_x,
+                            y - camera.top_y,
+                            WHITE,
+                            BLACK,
+                            to_cp437('.'),
+                        ),
+                        TileType::Wall => ctx.set(
+                            x - camera.left_x,
+                            y - camera.top_y,
+                            WHITE,
+                            BLACK,
+                            to_cp437('#'),
+                        ),
+                    }
+                }
+            }
+        }
+    }
 }
